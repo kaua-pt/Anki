175e9eed01e4751052ebe2e0cb38e0ac4e276fdb
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2022-06-25 23:08:58 -0300

    25/06

--- Backend/controllers/questoes.js
+++ Backend/controllers/questoes.js
@@ -25,4 +25,71 @@ const deletar = (req, res) => {
     })
 }
 
-module.exports = { cadastro, deletar }
+const mudarHidden = (req, res) => {
+    Questao.findOne({ _id: req.body.id }).then((questao) => {
+        if (questao) {
+            questao.hidden = req.params.hidden
+            questao.save()
+        } else {
+            res.send("Não foi possível mudar o elemento escondido desta questão")
+        }
+    })
+}
+
+const mudarDif = (req, res) => {
+    Questao.findOne({ _id: req.body.id }).then((questao) => {
+        if (questao) {
+            questao.dificuldade = req.body.dificuldade
+            questao.save()
+        } else {
+            res.send("Não foi possível mudar a dificuldade desta questão")
+        }
+    })
+}
+
+const pegar = (req, res) => {
+
+    function gerarRandom(min = 1, max = 10) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function pegarDado(dif) {
+        let cont = 0
+
+        while (1) {
+            let difFinal = dif + cont
+
+            Questao.find({ dificuldade: difFinal }).then((questoes) => {
+                if (questoes) {
+                    return res.send({ resultado: questoes })
+                }
+            })
+
+            if (difFinal == 5) {
+                cont = 1
+                dif = 0
+            } else {
+                cont = cont + 1
+            }
+        }
+    }
+
+    num = gerarRandom()
+
+    if (num <= 5) {
+        pegarDado(1)
+    } else if (num <= 20) {
+        pegarDado(2)
+    } else if (num <= 35) {
+        pegarDado(3)
+    } else if (num <= 65) {
+        pegarDado(4)
+    } else if (num <= 100) {
+        pegarDado(5)
+    }
+
+}
+
+module.exports = { cadastro, deletar, mudarHidden, mudarDif, pegar }
--- Backend/routes/questoes.js
+++ Backend/routes/questoes.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 const router = express.Router()
 const controllerQuestoes = require("../controllers/questoes")
 
+router.post("/pegarQuestao", controllerQuestoes.pegar)
 router.post("/cadastrarQuestao", controllerQuestoes.cadastro)
 router.delete("/excluirQuestao", controllerQuestoes.deletar)
+router.patch("/mudarHidden/:hidden", controllerQuestoes.mudarHidden)
+router.patch("/mudarDif", controllerQuestoes.mudarDif)
 
 module.exports = router
